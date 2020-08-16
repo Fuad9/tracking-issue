c57948604efee8a511df26a63774a3d9595dafc8
Fuad9 <alfuad8548@gmail.com>
2020-08-16 22:19:52 +0600

    tracking issue done

---
+++ instant.js
@@ -0,0 +1,2 @@
+const arr = [2, 4, 5, 3];
+console.log(arr.length);
--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function submitIssue(e) {
 
 const closeIssue = (id) => {
     const issues = JSON.parse(localStorage.getItem("issues"));
-    const currentIssue = issues.find((issue) => issue.id === id);
+    const currentIssue = issues.find((issue) => issue.id == id);
     currentIssue.status = "Closed";
     localStorage.setItem("issues", JSON.stringify(issues));
     fetchIssues();
@@ -33,26 +33,46 @@ const closeIssue = (id) => {
 
 const deleteIssue = (id) => {
     const issues = JSON.parse(localStorage.getItem("issues"));
-    const remainingIssues = issues.filter(issue.id !== id);
+    const remainingIssues = issues.filter((issue) => issue.id != id);
     localStorage.setItem("issues", JSON.stringify(remainingIssues));
+    fetchIssues();
 };
 
 const fetchIssues = () => {
-    const issues = JSON.parse(localStorage.getItem("issues"));
+    let issues = [];
+    if (localStorage.getItem("issues")) {
+        issues = JSON.parse(localStorage.getItem("issues"));
+    }
     const issuesList = document.getElementById("issuesList");
     issuesList.innerHTML = "";
+
+    let openIssue = 0;
 
     for (var i = 0; i < issues.length; i++) {
         const { id, description, severity, assignedTo, status } = issues[i];
 
+        let title = description;
+        if (status === "Open") {
+            openIssue++;
+        } else {
+            title = `<del> ${description}</del>`;
+        }
+
         issuesList.innerHTML += `<div class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status} </span></p>
-                              <h3> ${description} </h3>
+                              <h3> ${title} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
                               <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
     }
+
+    //Show total Issue
+    const totalIssue = document.getElementById("totalIssue");
+    totalIssue.innerText = issues.length;
+    // Show Issue currently open
+    const openIssueAmount = document.getElementById("openIssue");
+    openIssueAmount.innerText = openIssue;
 };
